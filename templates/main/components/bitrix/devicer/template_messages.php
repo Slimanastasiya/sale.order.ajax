6c2d9d2ac48a9d102088de8533c48a4bca9e25d8
Anastasia Slimak <slimakanastasia@gmail.com>
2020-07-30 18:42:02 +0300

    initial commit

---
+++ templates/main/components/bitrix/devicer/template_messages.php
@@ -0,0 +1,209 @@
+<?php
+use Bitrix\Main,
+    Bitrix\Main\Localization\Loc;
+
+$useDefaultMessages = !isset($arParams['USE_CUSTOM_MAIN_MESSAGES']) || $arParams['USE_CUSTOM_MAIN_MESSAGES'] != 'Y';
+
+if ($useDefaultMessages || !isset($arParams['MESS_AUTH_BLOCK_NAME']))
+{
+    $arParams['MESS_AUTH_BLOCK_NAME'] = Loc::getMessage('AUTH_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_REG_BLOCK_NAME']))
+{
+    $arParams['MESS_REG_BLOCK_NAME'] = Loc::getMessage('REG_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_BASKET_BLOCK_NAME']))
+{
+    $arParams['MESS_BASKET_BLOCK_NAME'] = Loc::getMessage('BASKET_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_REGION_BLOCK_NAME']))
+{
+    $arParams['MESS_REGION_BLOCK_NAME'] = Loc::getMessage('REGION_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PAYMENT_BLOCK_NAME']))
+{
+    $arParams['MESS_PAYMENT_BLOCK_NAME'] = Loc::getMessage('PAYMENT_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_DELIVERY_BLOCK_NAME']))
+{
+    $arParams['MESS_DELIVERY_BLOCK_NAME'] = Loc::getMessage('DELIVERY_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_BUYER_BLOCK_NAME']))
+{
+    $arParams['MESS_BUYER_BLOCK_NAME'] = Loc::getMessage('BUYER_BLOCK_NAME_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_BACK']))
+{
+    $arParams['MESS_BACK'] = Loc::getMessage('BACK_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_FURTHER']))
+{
+    $arParams['MESS_FURTHER'] = Loc::getMessage('FURTHER_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_EDIT']))
+{
+    $arParams['MESS_EDIT'] = Loc::getMessage('EDIT_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_MORE_DETAILS']))
+{
+    $arParams['MESS_MORE_DETAILS'] = Loc::getMessage('MORE_DETAILS_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_ORDER']))
+{
+    $arParams['MESS_ORDER'] = $arParams['~MESS_ORDER'] = Loc::getMessage('ORDER_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PRICE']))
+{
+    $arParams['MESS_PRICE'] = Loc::getMessage('PRICE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PERIOD']))
+{
+    $arParams['MESS_PERIOD'] = Loc::getMessage('PERIOD_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_NAV_BACK']))
+{
+    $arParams['MESS_NAV_BACK'] = Loc::getMessage('NAV_BACK_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_NAV_FORWARD']))
+{
+    $arParams['MESS_NAV_FORWARD'] = Loc::getMessage('NAV_FORWARD_DEFAULT');
+}
+
+$useDefaultMessages = !isset($arParams['USE_CUSTOM_ADDITIONAL_MESSAGES']) || $arParams['USE_CUSTOM_ADDITIONAL_MESSAGES'] != 'Y';
+
+if ($useDefaultMessages || !isset($arParams['MESS_PRICE_FREE']))
+{
+    $arParams['MESS_PRICE_FREE'] = Loc::getMessage('PRICE_FREE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_ECONOMY']))
+{
+    $arParams['MESS_ECONOMY'] = Loc::getMessage('ECONOMY_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_REGISTRATION_REFERENCE']))
+{
+    $arParams['MESS_REGISTRATION_REFERENCE'] = Loc::getMessage('REGISTRATION_REFERENCE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_AUTH_REFERENCE_1']))
+{
+    $arParams['MESS_AUTH_REFERENCE_1'] = Loc::getMessage('AUTH_REFERENCE_1_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_AUTH_REFERENCE_2']))
+{
+    $arParams['MESS_AUTH_REFERENCE_2'] = Loc::getMessage('AUTH_REFERENCE_2_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_AUTH_REFERENCE_3']))
+{
+    $arParams['MESS_AUTH_REFERENCE_3'] = Loc::getMessage('AUTH_REFERENCE_3_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_ADDITIONAL_PROPS']))
+{
+    $arParams['MESS_ADDITIONAL_PROPS'] = Loc::getMessage('ADDITIONAL_PROPS_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_USE_COUPON']))
+{
+    $arParams['MESS_USE_COUPON'] = Loc::getMessage('USE_COUPON_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_COUPON']))
+{
+    $arParams['MESS_COUPON'] = Loc::getMessage('COUPON_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PERSON_TYPE']))
+{
+    $arParams['MESS_PERSON_TYPE'] = Loc::getMessage('PERSON_TYPE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_SELECT_PROFILE']))
+{
+    $arParams['MESS_SELECT_PROFILE'] = Loc::getMessage('SELECT_PROFILE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_REGION_REFERENCE']))
+{
+    $arParams['MESS_REGION_REFERENCE'] = Loc::getMessage('REGION_REFERENCE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PICKUP_LIST']))
+{
+    $arParams['MESS_PICKUP_LIST'] = Loc::getMessage('PICKUP_LIST_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_NEAREST_PICKUP_LIST']))
+{
+    $arParams['MESS_NEAREST_PICKUP_LIST'] = Loc::getMessage('NEAREST_PICKUP_LIST_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_SELECT_PICKUP']))
+{
+    $arParams['MESS_SELECT_PICKUP'] = Loc::getMessage('SELECT_PICKUP_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_SELECTED_PICKUP']))
+{
+    $arParams['MESS_SELECTED_PICKUP'] = Loc::getMessage('SELECTED_PICKUP_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_INNER_PS_BALANCE']))
+{
+    $arParams['MESS_INNER_PS_BALANCE'] = Loc::getMessage('INNER_PS_BALANCE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_ORDER_DESC']))
+{
+    $arParams['MESS_ORDER_DESC'] = Loc::getMessage('ORDER_DESC_DEFAULT');
+}
+
+$useDefaultMessages = !isset($arParams['USE_CUSTOM_ERROR_MESSAGES']) || $arParams['USE_CUSTOM_ERROR_MESSAGES'] != 'Y';
+
+if ($useDefaultMessages || !isset($arParams['MESS_PRELOAD_ORDER_TITLE']))
+{
+    $arParams['MESS_PRELOAD_ORDER_TITLE'] = Loc::getMessage('PRELOAD_ORDER_TITLE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_SUCCESS_PRELOAD_TEXT']))
+{
+    $arParams['MESS_SUCCESS_PRELOAD_TEXT'] = Loc::getMessage('SUCCESS_PRELOAD_TEXT_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_FAIL_PRELOAD_TEXT']))
+{
+    $arParams['MESS_FAIL_PRELOAD_TEXT'] = Loc::getMessage('FAIL_PRELOAD_TEXT_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_DELIVERY_CALC_ERROR_TITLE']))
+{
+    $arParams['MESS_DELIVERY_CALC_ERROR_TITLE'] = Loc::getMessage('DELIVERY_CALC_ERROR_TITLE_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_DELIVERY_CALC_ERROR_TEXT']))
+{
+    $arParams['MESS_DELIVERY_CALC_ERROR_TEXT'] = Loc::getMessage('DELIVERY_CALC_ERROR_TEXT_DEFAULT');
+}
+
+if ($useDefaultMessages || !isset($arParams['MESS_PAY_SYSTEM_PAYABLE_ERROR']))
+{
+    $arParams['MESS_PAY_SYSTEM_PAYABLE_ERROR'] = Loc::getMessage('PAY_SYSTEM_PAYABLE_ERROR_DEFAULT');
+}
